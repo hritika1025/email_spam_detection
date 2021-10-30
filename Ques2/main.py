5c022210929cff671d74fc412c7b27275fe4adf4
Mitika <mitikabhadada03@gmail.com>
2021-10-30 15:14:42 +0530

    did ques2

---
+++ Ques2/main.py
@@ -0,0 +1,52 @@
+from flask import Flask ,render_template, session, url_for, request
+import sqlite3 as sql
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+from collections import Counter
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+
+app = Flask(__name__, template = 'template', static = 'static')
+
+
+app.secret_key='#the#secret#key#'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mitika@03'
+app.config['MYSQL_DB'] = 'spam_test'
+
+mysql = MySQL(app)
+print(mysql)
+@app.route("/", methods = ['GET', 'POST'])
+def home():
+    msg = ''
+    if request.method == 'POST' :
+        mail = request.form['mail']
+        if len(mail) > 0:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('INSERT INTO email VALUES (NULL, % s)',(mail,))
+            mysql.connection.commit()
+            cursor.close()
+            session['mail'] = mail
+        df = pd.read_csv('SMSSpamCollection', delimiter='\t',header=None)
+
+        X_train_raw, X_test_raw, y_train, y_test = train_test_split(df[1],df[0])
+
+        vectorizer = TfidfVectorizer()
+        X_train = vectorizer.fit_transform(X_train_raw)
+        classifier = LogisticRegression()
+        classifier.fit(X_train, y_train)
+
+        X_test = vectorizer.transform( [mail] )
+        prediction = classifier.predict(X_test)
+        msg = prediction
+
+
+
+    return render_template('home.html', msg = msg)
+
+app.run(debug=True)
---
+++ Ques2/spam_test.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE spam_test;
+USE spam_test;
+CREATE TABLE email(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    mail VARCHAR NOT NULL,
+    PRIMARY KEY(id)
+);
